9de0ed7ccd141f84305b1b823271553918e3e197
Jeimy Rodriguez <jeimyrodriguez@jeimys-mbp.home>
2020-05-04 21:03:22 -0700

    final changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function primeNum(){
+/*function primeNum(){
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
 
     // check if the numbers are in criteria
     if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100){
-        document.getElementById("message").innerHTML = "Invalid input, please try again.";
+    document.getElementById("message").innerHTML = "Invalid input, please try again.";
     }
     else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100){
         calculatePrimeNum(num1,num2);
-        document.getElementById("message").innerHTML = "";
+     
     }
 }
 function isPrime(num){
@@ -22,8 +22,7 @@ function isPrime(num){
     return res;
 }
 function calculatePrimeNum(firstNum,secondNum){
-    //alert("calculating");
-    var pNumArray = [];
+    var NumArray = [];
     var sNum = 0;
     var bNum = 0;
 
@@ -46,7 +45,7 @@ function calculatePrimeNum(firstNum,secondNum){
         console.log(isPrime(3));
         if(isPrime(sNum) == true){
             console.log(sNum);
-            pNumArray.push(sNum);
+            NumArray.push(sNum);
             sNum = sNum + 1;
         }
         else if (isPrime(sNum) == false){
@@ -54,7 +53,66 @@ function calculatePrimeNum(firstNum,secondNum){
             sNum = sNum + 1;
         }
     }
-    console.log(pNumArray);
-    document.getElementById("primeNumber").innerHTML = "There are " + pNumArray.length +" prime numbers.";
-    document.getElementById("message").innterHTML = pNumArray;
+    console.log(NumArray);
+    document.getElementById("primeNumumber").innerHTML = "There are " + NumArray.length +" prime numbers";
+    document.getElementById("message").innterHTML = NumArray;
+}*/ 
+
+function primeNum(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    // check if the numbers are within criteria
+    if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100){
+        document.getElementById("message").innerHTML = "Invalid input, please try again.";
+    }
+    else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100){
+        calculatePrimeNum(num1,num2);
+    }
+}
+function isPrime(num){
+    var res = true;
+    for (var i=2; i<=Math.ceil(num/2); i++){
+        if((num%i)==0){
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+function calculatePrimeNum(firstNum,secondNum){
+    var numArray = [];
+    var sNum = 0;
+    var bNum = 0;
+
+
+    if (firstNum > secondNum){
+        sNum = secondNum;
+        bNum = firstNum;
+    }
+    else if (firstNum < secondNum){
+        sNum = firstNum;
+        bNum = secondNum;
+    }
+
+    console.log(sNum);
+    console.log(bNum);
+
+    while(sNum <= bNum){
+        console.log("in while");
+
+        console.log(isPrime(3));
+        if(isPrime(sNum) == true){
+            console.log(sNum);
+            numArray.push(sNum);
+            sNum = sNum + 1;
+        }
+        else if (isPrime(sNum) == false){
+            console.log(sNum);
+            sNum = sNum + 1;
+        }
+    }
+    console.log(numArray);
+    document.getElementById("primeNumber").innerHTML = "There are " + numArray.length +" prime numbers.";
+    document.getElementById("message").innerHTML = numArray;
 }
